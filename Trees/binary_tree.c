65d8a05bf4dd9be48e676d40a4efe54028a72877
saber986 <saberkabir123@gmail.com>
2026-08-02 22:17:48 +0600

    AVL tree implementation

--- Trees/binary_tree.c
+++ Trees/binary_tree.c
@@ -1,32 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+typedef struct Node {
     int data;
-    struct Node* left;
-    struct Node* right;
-};
+    struct Node *left;
+    struct Node *right;
+    int height;
+} Node;
 
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
 
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+int getHeight(Node *node) {
+    if (node == NULL)
+        return 0;
+    return node->height;
+}
+
+Node* createNode(int value) {
+    Node *newNode = (Node*)malloc(sizeof(Node));
     newNode->data = value;
     newNode->left = NULL;
     newNode->right = NULL;
+    newNode->height = 1;
     return newNode;
 }
 
-
-void inorder(struct Node* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
-    }
+int getBalance(Node *node) {
+    if (node == NULL)
+        return 0;
+    return getHeight(node->left) - getHeight(node->right);
 }
 
+Node* rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
 
-void preorder(struct Node* root) {
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+    return y;
+}
+
+Node* insert(Node *node, int value) {
+    if (node == NULL)
+        return createNode(value);
+
+    if (value < node->data)
+        node->left = insert(node->left, value);
+    else if (value > node->data)
+        node->right = insert(node->right, value);
+    else
+        return node;
+
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+
+    int balance = getBalance(node);
+
+    // Left Left Case
+    if (balance > 1 && value < node->left->data)
+        return rightRotate(node);
+
+    // Right Right Case
+    if (balance < -1 && value > node->right->data)
+        return leftRotate(node);
+
+    // Left Right Case
+    if (balance > 1 && value > node->left->data) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // Right Left Case
+    if (balance < -1 && value < node->right->data) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void preorder(Node *root) {
     if (root != NULL) {
         printf("%d ", root->data);
         preorder(root->left);
@@ -34,34 +105,18 @@ void preorder(struct Node* root) {
     }
 }
 
-
-void postorder(struct Node* root) {
-    if (root != NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
-}
-
 int main() {
-    
+    Node *root = NULL;
 
-    struct Node* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
-    root->left->left = createNode(4);
-    root->left->right = createNode(5);
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
 
-    printf("Inorder Traversal:   ");
-    inorder(root);
-    printf("\n");
-
-    printf("Preorder Traversal:  ");
+    printf("Preorder traversal of AVL Tree: ");
     preorder(root);
-    printf("\n");
-
-    printf("Postorder Traversal: ");
-    postorder(root);
     printf("\n");
 
     return 0;
